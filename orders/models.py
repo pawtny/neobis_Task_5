cd30af42ba4e5f2ccdeb80d1d24c1e75cc1102ac
pawtny <sultanaliev_s@auca.kg>
2020-09-02 14:03:43 +0600

    test

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-# from meals.models import Meal
-# from users.models import User
+from meals.models import Meal
+from users.models import User
 
 class ServicePercentage(models.Model):
     percentage = models.IntegerField("percentage for the service", null = False, unique = True, primary_key = True)
@@ -11,12 +11,12 @@ class Status(models.Model):
 class Table(models.Model):
     name = models.CharField("name of the table", max_length = 50)
 
-# class Order(models.Model):
-#     waiterid = models.ForeignKey("id of the user", settings.AUTH_USER_MODEL, null = True, on_delete = models.SET_NULL)
-#     tableid = models.ForeignKey("id of the table", Table, null = True, on_delete = models.SET_NULL)
-#     tablename = models.ForeignKey("name of the table", Table, to_field = name, null = True, on_delete = models.SET_NULL)
-#     isitopen
-#     date
-#     mealsid = models.ForeignKey("id of the meals", Meal, null = True, on_delete = models.SET_NULL)
+class Order(models.Model):
+    waiterid = models.ManyToManyField("id of the user", User, null = True, on_delete = models.SET_NULL)
+    tableid = models.ForeignKey("id of the table", Table, null = True, on_delete = models.SET_NULL)
+    tablename = models.ForeignKey("name of the table", Table, to_field = name, null = True, on_delete = models.SET_NULL)
+    # isitopen
+    date = models.DateTimeField("date of creation", auto_now_add=True)
+    mealsid = models.ManyToManyField("id of the meals", Meal, on_delete = models.CASCADE)
 
 
